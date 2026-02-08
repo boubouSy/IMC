e0109a19ebeee6907eb34f3ff05811ee79be7687
boubouSy <bouboujaddel19@gmail.com>
2026-02-09 00:17:36 +0100

    Add files via upload

---
+++ Projet1.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+with st.container(border=True):
+    st.title("Calcul des IMC")
+
+poids = st.number_input("Entrez votre poids")
+taille = st.number_input("Entrez votre taille")
+
+if st.button("Calculer"):
+    
+    IMC=poids/(taille**2)
+    st.write("L'IMC est de", IMC)
+    
+    if IMC < 18.5:
+        st.warning("Vous étes maigre.")
+        
+    elif 18.5 < IMC < 25:
+        st.success("Vous avez un poids normal.")
+        
+    elif 25 < IMC < 30:
+        st.warning("Attention ! Vous étes en surpoids.")
+
+    else :
+        st.write("Votre catégorie n'est pas prise en compte.")
